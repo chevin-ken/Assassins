93f1db2d1255c44b1fb0c310492deffc6b41e237
Kevin <chevin_ken@berkeley.edu>
2019-04-08 23:16:38 -0700

    Updated assassins file

--- assassins.js
+++ assassins.js
@@ -1,1 +1,25 @@
-let games = new Map();
+admins = new Map()
+
+players = new Map()
+
+registerUser(username, password, admin) {
+	if (admin) {
+		if (!admin.hasKey(username)) {
+			admin.set(username, new User(username, password, true))
+		}
+		else {
+			return "Username Taken"
+		}
+	}
+	else {
+		if (!players.hasKey(username)) {
+			players.set(username, new User(username, password, false))
+		}
+		else {
+			return "Username Taken"
+		}
+	}
+}
+
+
+
