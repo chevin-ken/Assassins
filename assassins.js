3d2174464df6138a43c35368276677ed6895f46a
Kevin <chevin_ken@berkeley.edu>
2019-05-06 01:53:04 -0700

    Changed stuff

--- assassins.js
+++ assassins.js
@@ -1,11 +1,16 @@
+//Admin account database
+
 let admins = new Map()
+
+//Player account database
 
 let players = new Map()
 
+//Registers a user given name, username, password, and whether or not it's an admin account
 const registerUser = function(name, username, password, admin) {
 	if (admin) {
-		if (!admin.hasKey(username)) {
-			admin.set(username, new User(name, username, password, true))
+		if (!admins.hasKey(username)) {
+			admins.set(username, new User(name, username, password, true))
 		}
 		else {
 			return "Username Taken"
@@ -20,6 +25,29 @@ const registerUser = function(name, username, password, admin) {
 		}
 	}
 }
+
+
+//Returns the user object associated with the login info, returns error string if login fails
+const login = function(username, password, admin) {
+	if (admin) {
+		if (!admins.hasKey(username) || (! admins.get(username).password === password)) {
+			return "Login failed";
+		}
+		else {
+			return admins.get(username);
+		}
+	}
+	else {
+		if (!players.hasKey(username) || (!players.get(username).password === password)) {
+			return "Login failed"
+		}
+		else {
+			return admins.get(username)
+		}
+	}
+}
+
+
 
 
 
